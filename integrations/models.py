290bf4681963a99048470b471123acbbdc153afe
Matteo Bucciol <bucciol.matteo@proton.me>
2025-10-24 14:34:19 +0200

    Fix - Create a YT Film Info Models

--- integrations/models.py
+++ integrations/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class YTFilmTitle(models.Model):
+    """Store YouTube video titles from a channel"""
+    title = models.CharField(max_length=255)
+    youtube_video_id = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.title
